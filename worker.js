f7d7bf16a4fd670eb5a280ecf3a4b8a61f1a0d44
admi8829 <habtamu.yifiru.official@gmail.com>
2026-04-26 04:24:09 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -23,7 +23,7 @@ const requestPhoneKeyboard = Markup.keyboard([
 ]).resize();
 
 // 3. የቋንቋ ምርጫ (Inline Button)
-* /*const languageKeyboard = Markup.inlineKeyboard([
+/*const languageKeyboard = Markup.inlineKeyboard([
   [Markup.button.callback('አማርኛ 🇪🇹', 'set_lang_am'), 
    Markup.button.callback('English 🇺🇸', 'set_lang_en')]
 ]);*/
